f615c55c2876079a1ab7d67b78a6e1ad9f5954c0
Galih Adhi Kusuma <noxturne.production@gmail.com>
2021-08-21 14:47:14 +0700

    reseting layout design and replace it with real design

--- app/app.js
+++ app/app.js
@@ -1,54 +1,88 @@
 // ALL VARIABLES
-const submitForm = document.querySelector("form")
-const userInputText = document.querySelector(".user-input")
-const botsChoice = document.querySelectorAll(".bots")
-const chatWindow = document.querySelector(".chatbox-window")
-const chatHeader = document.querySelector(".chat-name h2")
-const botWrapper = document.querySelector(".choose-bot-wrapper")
-const botsContainer = document.querySelector(".bots-container")
-const yukSinauBtn = document.querySelector(".btn-belajar")
-const closeBtn = document.querySelector(".close-btn")
-const closeChatBtn = document.querySelector(".close-chat")
-const botChoiceContainerHeader = document.querySelector(".bots-container .header")
-const loader = document.querySelector(".loader-container")
+// const submitForm = document.querySelector("form")
+// const userInputText = document.querySelector(".user-input")
+// const botsChoice = document.querySelectorAll(".bots")
+// const chatWindow = document.querySelector(".chatbox-window")
+// const chatHeader = document.querySelector(".chat-name h2")
+// const botWrapper = document.querySelector(".choose-bot-wrapper")
+// const botsContainer = document.querySelector(".bots-container")
+// const yukSinauBtn = document.querySelector(".btn-belajar")
+// const closeBtn = document.querySelector(".close-btn")
+// const closeChatBtn = document.querySelector(".close-chat")
+// const botChoiceContainerHeader = document.querySelector(".bots-container .header")
+// const loader = document.querySelector(".loader-container")
+// const body = document.querySelector("body")
 
-window.addEventListener("load", () => {
-    setTimeout(()=> {
-        loader.classList.add("wipe-left")
-        loader.classList.add("hidden")
-        console.log("anjay")
-    },4000)
-})
+// window.addEventListener("load", () => {
+//     body.classList.add("no-scroll")
+//     window.scrollTo(0, 0)
+
+//     setTimeout(() => {
+//         loader.classList.add("wipe-left")
+//         body.classList.remove("no-scroll")
+//     }, 4000)
+// })
+
+// window.addEventListener("scroll", () => {
+//     const aboutHeader = document.querySelector("#aboutPage")
+
+//     const rect = aboutHeader.getBoundingClientRect().top;
+//     console.log(rect + window.scrollY)
+// })
+
+// botsChoice.forEach(bot => {
+//     bot.addEventListener("click", () => {
+//         if (chatWindow.dataset.hidden == "true") {
+//             chatWindow.dataset.hidden = "false"
+//         }
+
+//         if (bot.dataset.language == "BHS_JAWA") {
+//             botBahasaJawa()
+//         } else if (bot.dataset.language == "BHS_SUNDA") {
+//             botBahasaSunda()
+//         }
+//     })
+// })
 
 
-botsChoice.forEach(bot => {
-    bot.addEventListener("click", () => {
-        if(chatWindow.dataset.hidden == "true"){
-            chatWindow.dataset.hidden = "false"
-        }
-          
-        if (bot.dataset.language == "BHS_JAWA") {
-            botBahasaJawa()
-        } else if (bot.dataset.language == "BHS_SUNDA") {
-            botBahasaSunda()
-        }
-    })
-})
+
+// yukSinauBtn.addEventListener("click", () => {
+//     botsContainer.removeAttribute("data-hidden")
+
+//     if (chatWindow.dataset.isActive == "true") {
+//         window.scroll
+//     }
+// })
+
+// closeBtn.addEventListener("click", () => {
+//     botsContainer.dataset.hidden = "true";
+// })
+
+// closeChatBtn.addEventListener("click", () => {
+//     chatWindow.dataset.hidden = "true"
+// })
+
+const navbar = document.querySelector(".navbar-top")
+const navbarIntersectTarget = document.querySelector(".landing-page-content")
+const logoNavbar = document.querySelector(".logo-navbar")
+
+let navbarObserverOption = {
+    root : document.querySelector("navbar"),
+    rootMargin : "200px",
+}
+
+// const observer = new IntersectionObserver( entries => {
+//     entries.forEach(entry => {
+//         if(!entry.isIntersecting){
+//             navbar.classList.add("navbar-top-scrolled")
+//             logoNavbar.src = "../assets/icon/logo-in-navbar-2.svg"
+//         } else {
+//             navbar.classList.remove("navbar-top-scrolled")
+//             logoNavbar.src = "../assets/icon/logo in navbar v2.svg"
+//         }
+//     })
+// }, navbarObserverOption)
+
+// observer.observe(navbarIntersectTarget)
 
 
-
-yukSinauBtn.addEventListener("click", () => {
-    botsContainer.removeAttribute("data-hidden")
-
-    if(chatWindow.dataset.isActive == "true"){
-        window.scroll
-    }
-})
-
-closeBtn.addEventListener("click", () => {
-    botsContainer.dataset.hidden = "true";
-})
-
-closeChatBtn.addEventListener("click", () => {
-    chatWindow.dataset.hidden = "true"
-})
