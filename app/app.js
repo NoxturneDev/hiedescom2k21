4ae4fb90c93310a4051d70e5df9dc880404f47be
Galih Adhi Kusuma <noxturne.production@gmail.com>
2021-09-05 14:59:22 +0700

    1 commit before importing prototype

--- app/app.js
+++ app/app.js
@@ -67,21 +67,21 @@
 //////////////////////////////////////////////////////////////
 const navbarLink = document.querySelectorAll(".nav-link a")
 
-function smoothScroll(e){  
+function smoothScroll(e) {
     e.preventDefault()
     const targetID = e.currentTarget.getAttribute("href")
     const margin = 100;
 
-    if(targetID === "#mainFeaturePage") {
+    if (targetID === "#mainFeaturePage") {
         window.scrollTo({
-           top: document.querySelector(targetID).offsetTop + (2* margin),
-           behavior: "smooth"
+            top: document.querySelector(targetID).offsetTop + (2 * margin),
+            behavior: "smooth"
         })
     } else {
         window.scrollTo({
             top: document.querySelector(targetID).offsetTop - margin,
             behavior: "smooth"
-         })
+        })
     }
 }
 
@@ -95,18 +95,18 @@ const closeNavIcon = document.querySelector("#close-nav")
 navbarLink.forEach(link => {
     link.addEventListener("click", smoothScroll)
 
-   
-    link.addEventListener("click", ()=> {
+
+    link.addEventListener("click", () => {
         const mediaQuery = window.matchMedia("(max-width: 768px)")
-// ! refactor above so u don't repeat the same funtcion at animateONscrollJS
+        // ! refactor above so u don't repeat the same funtcion at animateONscrollJS
         mediaQuery.addListener(navViewport)
         navViewport(mediaQuery, link)
     })
-    
+
 })
 
-function navViewport(e, link){
-    if(e.matches){
+function navViewport(e, link) {
+    if (e.matches) {
         navToggle(link)
     } else {
 
@@ -125,13 +125,13 @@ navToggleIcon.forEach(icon => {
 })
 
 
-function navToggle(){
+function navToggle() {
     let toggleStatus = navbarContainer.dataset.toggle
 
     // add smooth transition
     navbarContainer.style.transition = "transform 1s ease-in-out"
 
-    if(toggleStatus == "false"){
+    if (toggleStatus == "false") {
         navbarContainer.style.transform = "translateX(0)"
         navbarContainer.dataset.toggle = "true"
     } else {
@@ -156,7 +156,7 @@ function navToggle(){
 //         loader.style.transform = "translateX(-100%)"
 //         document.body.style.overflow = "";
 //     }, 3000)
-  
+
 //     // loader.addEventListener("animationend", ()=> {
 //     //     loader.classList.remove("wipe-right")
 //     // })
@@ -164,29 +164,50 @@ function navToggle(){
 // })
 
 
-const featuresImg = document.querySelectorAll(".feature-redirect-img")
+//////////////////////////////////////////////////////////////
+// FEATURE 3D DIMENSION 
+//////////////////////////////////////////////////////////////
+const dimensionWrapper = document.querySelector(".dimension-wrapper")
+const featureOn3D = Array.from(document.querySelectorAll(".container-dimension"))
+const btnRedirect = document.querySelectorAll(".container-dimension .btn-rotate")
 
-// featuresImg.forEach(img => {
-//     if(img.classList.contains("chat-redirect")){
-//         img.addEventListener("mouseenter", () => {
-//             // chatRedirectImg.setAttribute.src = "../assets/animation/Nau-jump-on-hover.gif"
-//             // img.style.background = "url("../assets/animation/Nau-jump-on-hover.gif")
-//             // setTimeout(()=> {
-                
-//             // }, 250)
-//             console.log("mouse enter")
-//             console.log(img)
-//         })
-//     } else {
-//         img.addEventListener("mouseleave", () => {
-//             img.setAttribute("src", "../assets/animation/Nau-jump-on-hover-reverse.gif")
-//             // setTimeout(()=> {
-                
-//             // }, 250)
-//             console.log("mouse leave")
-//         })
-//     }
-// })
+featureOn3D.forEach((feature, index) => {
+    feature.style.transform = `rotateY(${index * 90}deg) translateZ(${window.innerWidth + 200}px)`
+})
+
+
+console.log(featureOn3D)
+
+btnRedirect.forEach(btn => {
+    btn.addEventListener("click", () => {
+        btnRotate(btn)
+    })
+})
+
+function btnRotate(btn) {
+    const btnId = btn.id
+    if (btnId == "toChat") {
+        console.log("ke kuis")
+        dimensionWrapper.style.transform = "rotateY(90deg)"
+        dimensionWrapper.dataset.animation = "rotate"
+
+    } else if (btnId == "toQuizWrapper") {
+        console.log("ke kuis")
+        dimensionWrapper.style.transform = "rotateY(-90deg)"
+        dimensionWrapper.dataset.animation = "rotate"
+    } else if (btnId == "toQuiz") {
+        dimensionWrapper.style.transform = "rotateY(-180deg)"
+        dimensionWrapper.dataset.animation = "rotate"
+    } else if (btnId == "toChatWrapper") {
+        console.log("ke kuis")
+        dimensionWrapper.style.transform = "rotateY(0)"
+        dimensionWrapper.dataset.animation = "rotate"
+    }
+
+    dimensionWrapper.addEventListener("transitionend", ()=> {
+        dimensionWrapper.dataset.animation = "none"
+    })
+}
 // !browser alwasy chache the image so it's okay to use src changing
 // !FIX ABOVE
 
@@ -204,27 +225,27 @@ const closeOverlayBtn = document.querySelectorAll(".close-overlay")
 const mascotWrapper = document.querySelector(".feature-wrapper")
 
 btnOverlay.forEach(btn => {
-        btn.addEventListener("mouseenter", e => {
-            btn.classList.add("wipe-up")
-        })
-    
-        btn.addEventListener("mouseleave", e => {
-            btn.classList.remove("wipe-up")
-            btn.classList.add("wipe-up-reverse")
-        })
-    
-        btn.addEventListener("animationend", () => {
-            if(btn.classList.contains("wipe-up-reverse")){
-                btn.classList.remove("wipe-up-reverse")
-            }
-        })
+    btn.addEventListener("mouseenter", e => {
+        btn.classList.add("wipe-up")
+    })
+
+    btn.addEventListener("mouseleave", e => {
+        btn.classList.remove("wipe-up")
+        btn.classList.add("wipe-up-reverse")
+    })
+
+    btn.addEventListener("animationend", () => {
+        if (btn.classList.contains("wipe-up-reverse")) {
+            btn.classList.remove("wipe-up-reverse")
+        }
+    })
 })
 
 
 buttonRedirect.forEach(btn => {
-    btn.addEventListener("click", (e)=> {
+    btn.addEventListener("click", (e) => {
         console.log(btn)
-        if(btn.id == "redirectBtnChat"){
+        if (btn.id == "redirectBtnChat") {
             chatWindowOverlay.style.transform = "translateX(0)"
             mascotWrapper.style.transform = "translateX(100%)"
             chatWindowOverlay.classList.add("fade-1")
@@ -234,11 +255,11 @@ buttonRedirect.forEach(btn => {
             quizWindowOverlay.classList.add("fade-1")
         }
 
-      
-        chatWindowOverlay.addEventListener("animationend", ()=>{
+
+        chatWindowOverlay.addEventListener("animationend", () => {
             chatWindowOverlay.classList.remove("fade-1")
         })
-        quizWindowOverlay.addEventListener("animationend", ()=>{
+        quizWindowOverlay.addEventListener("animationend", () => {
             quizWindowOverlay.classList.remove("fade-1")
         })
 
@@ -247,7 +268,7 @@ buttonRedirect.forEach(btn => {
         mascotWrapper.classList.add("fade-0")
 
 
-        mascotWrapper.addEventListener("animationend", ()=> {
+        mascotWrapper.addEventListener("animationend", () => {
             mascotWrapper.classList.remove("fade-0")
         })
         document.body.style.overflow = "hidden";
@@ -256,7 +277,7 @@ buttonRedirect.forEach(btn => {
 
 closeOverlayBtn.forEach(btn => {
     btn.addEventListener("click", (e) => {
-        if(e.target.id == "closeChatOverlay"){
+        if (e.target.id == "closeChatOverlay") {
             chatWindowOverlay.style.transform = "translateX(-100%)"
             chatWindowOverlay.classList.add("fade-0")
         } else {
@@ -264,17 +285,17 @@ closeOverlayBtn.forEach(btn => {
             quizWindowOverlay.classList.add("fade-0")
         }
 
-     
 
-        chatWindowOverlay.addEventListener("animationend", ()=>{
+
+        chatWindowOverlay.addEventListener("animationend", () => {
             chatWindowOverlay.classList.remove("fade-0")
         })
-        quizWindowOverlay.addEventListener("animationend", ()=>{
+        quizWindowOverlay.addEventListener("animationend", () => {
             quizWindowOverlay.classList.remove("fade-0")
         })
 
         mascotWrapper.classList.add("fade-1")
-        mascotWrapper.addEventListener("animationend", ()=> {
+        mascotWrapper.addEventListener("animationend", () => {
             mascotWrapper.classList.remove("fade-1")
         })
 
